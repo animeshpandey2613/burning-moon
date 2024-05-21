6727d91f27fcc008d45b61e15bd9eb692f16e18f
animeshpandey2613 <121745585+animeshpandey2613@users.noreply.github.com>
2024-05-21 09:42:22 +0530

    media query commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ResetPassword from "./pages/ResetPassword";
 import Protected from "./utils/Protected";
 import Logout from "./pages/Logout";
 import { Helmet } from "react-helmet";
-// import Media from "react-media";
+import Media from "react-media";
 import Logo from "./images/Logo.png";
 function App() {
   return (
@@ -19,15 +19,15 @@ function App() {
         <title>Burning Moon</title>
         <link rel="icon" type="image/png" href={Logo} />
       </Helmet>
-      {/* <Media query="(max-width:1200px)"> */}
-        {/* {(matches) => {
+      <Media query="(max-width:1200px)">
+        {(matches) => {
           return matches ? (
             <h2 className="heading2">
               Mobile version of this site is under construction, please open it
               on your PC OR visit my linkedIn profile -
               https://www.linkedin.com/in/animesh-pandey-00a3bb228/
             </h2>
-          ) : ( */}
+          ) : (
             <Router>
               <Routes>
                 <Route path="/user" element={<Protected />}>
@@ -45,9 +45,9 @@ function App() {
                 <Route path="/logout" element={<Logout />} />
               </Routes>
             </Router>
-          {/* );
-        }} */}
-      {/* </Media> */}
+           );
+        }} 
+       </Media> 
     </div>
   );
 }
